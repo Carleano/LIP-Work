7a675c687daada4a16131ed75981de5d9e94701a
Carleano <52502287+Carleano@users.noreply.github.com>
2019-07-09 10:42:21 -0400

    Update avg_val_pic_func.py

--- avg_val_pic_func.py
+++ avg_val_pic_func.py
@@ -5,13 +5,15 @@ def avg_val_pic(img, numtiles,reshape_x,reshape_y): #input img as string
 	import image_slicer
 	from PIL import Image
 	from skimage.filters import sobel
-	
+	from matplotlib import cm
 	
 	
 	imag = Image.open(img)
+	imag_vals = (img_as_float(imag))
 	image = sobel(img_as_float(imag))# Applies Sobel filter to image
-	if (image >= 0.9*image).any() : 0
-	tiles = image_slicer.slice(img, numtiles, save=False)# Tiles the images into numtiles
+	sob_rep_ind = np.where(.125*imag_vals <= image)
+	image[sob_rep_ind[0],sob_rep_ind[1]] = 0
+	tiles = image_slicer.slice(img, numtiles, save=False)# Tiles the image into numtiles
 	strg_arr = np.zeros((numtiles), dtype=np.uint16)  # storage array of zeros
 	
 	for f,tile in enumerate(tiles): 
@@ -20,5 +22,5 @@ def avg_val_pic(img, numtiles,reshape_x,reshape_y): #input img as string
 		strg_arr[f] = pixels 		# sets all values in empty array to mean of the tile
 	
 	strg_arr = strg_arr.reshape(reshape_x, reshape_y)
-	#plt.imshow(strg_arr, origin='lower', vmin=strg_arr.std(), vmax=strg_arr.max())##show new image
-	#plt.show()
+	#plt.imshow(strg_arr, origin='lower', vmin=strg_arr.std(), vmax=strg_arr.max(), cmap='cividis')	
+	plt.imshow(image, origin = 'lower', vmin = image.min(), vmax = image.max(), cmap='viridis')
