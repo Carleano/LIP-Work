a60f6eba0b6518a44eda536956a2f045b07efefc
Carleano <52502287+Carleano@users.noreply.github.com>
2019-07-12 13:20:01 -0400

    Update filter_test.py

--- filter_test.py
+++ filter_test.py
@@ -1,35 +1,37 @@
+#Shows original images and the images log Scaled side by side
 import numpy as np
+from astropy.io import fits	
 import matplotlib.pyplot as plt
 
 from PIL import Image
 from skimage.filters import roberts, sobel, sobel_h, sobel_v, scharr, \
     scharr_h, scharr_v, prewitt, prewitt_v, prewitt_h
-    
-img=Image.open('2019_06_14__00_00_12_63__SDO_AIA_AIA_335.png')
-image = img
-edge_roberts = roberts(image)
-edge_sobel = sobel(image)
-edge_scharr = scharr(image)
-edge_prewitt = prewitt(image)
+ 
+ 
+#Original data 
+inact_image = fits.getdata('aia.lev1.94A_2019-06-14T00_00_35.12Z.image_lev1.fits')    
+active_image = fits.getdata('aia.lev1.94A_2012-07-11T01_17_13.13Z.image_lev1.fits')
+#Log Scale the images
+log_inct = np.log10(fits.getdata('aia.lev1.94A_2019-06-14T00_00_35.12Z.image_lev1.fits'))
+log_actv = np.log10(fits.getdata('aia.lev1.94A_2012-07-11T01_17_13.13Z.image_lev1.fits'))
 
-fig, ax = plt.subplots(ncols=4, sharex=True, sharey=True,
+#Setup Subplot figure for images
+fig, ax = plt.subplots(nrows=2,ncols=2, sharex=True, sharey=True,
                        figsize=(20, 10))
 
-ax[0].imshow(edge_roberts, cmap=plt.cm.inferno)
-ax[0].set_title('Roberts Edge Detection')
+ax[0,0].imshow(inact_image,origin = 'lower', vmin=1,vmax=2.95, cmap=plt.cm.inferno)
+ax[0,0].set_title('Original Inactive')
 
-ax[1].imshow(edge_sobel, cmap=plt.cm.inferno)
-ax[1].set_title('Sobel Edge Detection')
+ax[1,0].imshow(active_image,origin = 'lower', vmin=1,vmax=2.95, cmap=plt.cm.inferno)
+ax[1,0].set_title('Orignal Active')
 
-ax[2].imshow(edge_scharr, cmap=plt.cm.inferno)
-ax[2].set_title('Scharr Edge Detection')
+ax[0,1].imshow(log_inct,origin = 'lower', vmin=1,vmax=2.95, cmap=plt.cm.inferno)
+ax[0,1].set_title('Base 10 inactive')
 
-ax[3].imshow(edge_prewitt, cmap=plt.cm.inferno)
-ax[3].set_title('Prewitt Edge Detection')
+ax[1,1].imshow(log_actv,origin = 'lower', vmin=1,vmax=2.95, cmap=plt.cm.inferno)
+ax[1,1].set_title('Base 10 Active')
 
 
-for a in ax:
-    a.axis('off')
 
 plt.tight_layout()
 plt.show()
