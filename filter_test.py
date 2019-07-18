93254932d0bb8019deacc18aa82d1a4d23c50c9f
Carleano <52502287+Carleano@users.noreply.github.com>
2019-07-18 10:57:06 -0400

    Update filter_test.py

--- filter_test.py
+++ filter_test.py
@@ -9,23 +9,21 @@ from skimage.filters import roberts, sobel, sobel_h, sobel_v, scharr, \
  
  
 #Original data 
-inact_image = fits.getdata('aia.lev1.94A_2019-06-14T00_00_35.12Z.image_lev1.fits')    
 active_image = fits.getdata('aia.lev1.94A_2012-07-11T01_17_13.13Z.image_lev1.fits')
+#Denoise
+dnsd_active = denoise_tv_chambolle(active_image, weight=50, multichannel=True)
 
 #Log Scale the images
-log_inct = np.log10(fits.getdata('aia.lev1.94A_2019-06-14T00_00_35.12Z.image_lev1.fits'))
-log_actv = np.log10(fits.getdata('aia.lev1.94A_2012-07-11T01_17_13.13Z.image_lev1.fits'))
-#
-Base_10_dnsd = denoise_tv_chambolle(log_actv, weight=50, multichannel=True)
-
+base_10_dnsd_act = np.log10(dnsd_active)
+base_10 = np.log10(active_image)
 #Setup Subplot figure for images
 fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True,
                        figsize=(20, 10))
 
-ax[0].imshow(log_actv, vmin=1,vmax=2.95, cmap=plt.cm.inferno )
+ax[0].imshow(base_10, vmin=1,vmax=2.95, cmap=plt.cm.inferno )
 ax[0].axis('off')
 ax[0].set_title('Base 10 Active Sun')
-ax[1].imshow(Base_10_dnsd, vmin=1,vmax=2.95, cmap=plt.cm.inferno)
+ax[1].imshow(base_dnsd_10_act, vmin=1,vmax=2.95, cmap=plt.cm.inferno)
 ax[1].axis('off')
 ax[1].set_title('Denoised Base 10 Active Sun')
 
